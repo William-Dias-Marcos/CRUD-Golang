e6fadfb79b4e27a09ee1f5ffa811ee11653aa336
William-Dias-Marcos <william.diasmarcos@gmail.com>
2025-07-10 22:13:27 -0300

    feat: creating user object and adding validations

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,12 +1,23 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/William-Dias-Marcos/CRUD-Golang/src/configuration/rest_err"
+	"github.com/William-Dias-Marcos/CRUD-Golang/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context){
 
-	err := rest_err.NewBadRequestError("Chamou errado")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindBodyWithJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s\n" , err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
 }
